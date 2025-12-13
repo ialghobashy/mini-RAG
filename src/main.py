faf1a082468bf7bfa2bf31d4271cb5f4ed7892d1
ialghobashy <ialghobashy@gmail.com>
2025-12-13 22:50:12 +0200

    upload file codes

--- src/main.py
+++ src/main.py
@@ -1,9 +1,11 @@
 from fastapi import FastAPI # type: ignore
 from routes import base
-
+from routes import data, data
 
 app = FastAPI()
 app.include_router(base.base_router)
+app.include_router(data.data_router)
+
 
 
 
--- src/routes/base.py
+++ src/routes/base.py
@@ -1,6 +1,6 @@
-from fastapi import FastAPI, APIRouter # type: ignore
+from fastapi import FastAPI, APIRouter, Depends # type: ignore
 import os
-from helpers.config import get_settings
+from helpers.config import get_settings,Settings
 
 base_router = APIRouter(
     prefix="/api/v1",
@@ -9,7 +9,7 @@ base_router = APIRouter(
 
 @base_router.get("/")
 
-async def welcome():
+async def welcome(app_settings: Settings =Depends(get_settings)):
     app_settings = get_settings()
     app_name = app_settings.APP_NAME
     app_version = app_settings.APP_VERSION
