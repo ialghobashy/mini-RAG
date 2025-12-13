58be16a67cc56b7e3714fad441cb7225a8e41f6d
ialghobashy <ialghobashy@gmail.com>
2025-12-13 16:33:22 +0200

    MVC stracture

--- src/main.py
+++ src/main.py
@@ -1,12 +1,8 @@
 from fastapi import FastAPI # type: ignore
-from dotenv import load_dotenv # type: ignore
 from routes import base
 
 
-load_dotenv(".env")
-
 app = FastAPI()
-
 app.include_router(base.base_router)
 
 
