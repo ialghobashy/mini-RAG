7d282c346bf37ba8df44b365139f9b9e073d3a24
ialghobashy <ialghobashy@gmail.com>
2025-12-15 22:52:36 +0200

    get or create project

--- src/models/ProjectModel.py
+++ src/models/ProjectModel.py
@@ -7,4 +7,20 @@ class ProjectModel(BaseDataModel):
         super().__init__(db_client)
         self.collection = self.db_client[DatabaseEnum.COLLECTION_PROJECT_NAME.value]
 
+    async def create_project(self, project: Project):
+
+        result = await self.collection.insert_one(project.dict())
+        project.id = result.inserted_id
+        return project
+    
+    async def get_project_or_create_one(self, project_id: str):
+
+        record = await self.collection.find_one({"project_id": project_id})
+
+        if record is None:
+            project = Project(project_id=project_id)
+            project = await self.create_project(project=project)
+            return project
+        return Project(**record)
+    
     
